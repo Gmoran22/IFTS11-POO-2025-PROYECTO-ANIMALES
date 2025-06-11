02f512e2d6445171648ea16d21641091fd6b9cd6
Joa6jb <joa6jb@gmail.com>
2025-06-11 18:54:09 -0300

    -

--- Joako/main.py
+++ Joako/main.py
@@ -1,156 +1,173 @@
-'''1. Clase Perro
-● Atributos: nombre, raza, edad, tamaño, peso, estado_salud, vacunado, estado
-('disponible', 'reservado', 'adoptado'), temperamento, id.
-● Métodos: cambiar estado, mostrar información, etc.'''
-
-class Perro(object):
-    """representa un perro en el sistema de adopción.
+class Perros(object):
     
-    almacena la información de un perro: su nombre, raza, edad,
-        tamaño, peso, estado de salud, estado en el sistema, etc.
-        con los metodos para cambiar su estado y mostrar su información
-
-        ej para usarlo:
-        perro = Perro("Gerardo", "chihuahua", 3, "Chiquito", 25, "Saludable", True, "disponible", "Amigable", 101)
-        perro.cambiarEstado("reservado")
-        perro.mostrar_informacion()
-    """
-    def __init__(self, nombre, raza, edad, tamaño, peso, estado_salud, vacunado, estado, temperamento, id):
+    def __init__(self, nombre, raza, edad, estado_salud, tamaño, vacunado, estado_adopcion, temperamento, id):
         self.nombre = nombre
         self.raza = raza
         self.edad = edad
-        self.tamaño = tamaño
-        self.peso = peso
         self.estado_salud = estado_salud
+        self.tamaño = tamaño
         self.vacunado = vacunado
-        self.estado = estado
+        self.estado_adopcion = estado_adopcion
         self.temperamento = temperamento
         self.id = id
-        
-    def cambiarEstado(self,nuevo_estado):
-        """cambia el estado del perro.
-        el estado tiene que ser "disponible", "reservado" o "adoptado"
-        
-        ej para usarlo:
-        perro.cambiarEstado("adoptado") """
-
-        if nuevo_estado in ["disponible", "reservado", "adoptado"]:
-            self.estado = nuevo_estado
+    
+    def cambiar_estado(self, nuevo_estado):
+        if nuevo_estado in ["disponible" , "reservado", "adoptado"]:
+            self.estado_adopcion = nuevo_estado
             print("Estado actualizado")
         else:
-            print("Estado inválido") 
-    
+            print("Estado invalido")
+
+
     def mostrar_informacion(self):
-        """
-        es para mostrar la informacion del perro en la consola
+        return  {
+            "Nombre": self.nombre,
+            "Raza": self.raza,
+            "Edad": self.edad,
+            "Estado de Salud": self.estado_salud,
+            "Tamaño": self.tamaño,
+            "Vacunado": self.vacunado,
+            "Estado de Adopción": self.estado_adopcion,
+            "Temperamento": self.temperamento,
+            "ID": self.id
+        }
         
-        ejemplo para usarlo:
-            perro.mostrar_informacion()
-        """
-        return {
-        "Nombre": self.nombre,
-        "Raza": self.raza,
-        "Edad": self.edad,
-        "Tamaño": self.tamaño,
-        "Peso": self.peso,
-        "Estado salud": self.estado_salud,
-        "Vacunado": self.vacunado,
-        "Estado": self.estado,
-        "Temperamento": self.temperamento,
-        "ID": self.id,
-    }
-
-
-'''2.Clase UsuarioAdoptante
-● Atributos: nombre, dni, email, preferencias (raza, edad, tamaño),
-historial_adopciones.
-● Métodos: registrarse, modificar datos, ver historial, etc.'''
 
 class UsuarioAdoptante(object):
-    
     def __init__(self, preferencias=None, historial_adopciones=None):
-        """
-        Pongo los datos con None porque en teoria todavia el usuario no se registro, entonces no hay datos"
-        
-        """
         
         self.nombre = None
-        self.apellido = None
+        self.apellido= None
         self.dni = None
-        self.email = None
-        self.telf = None
+        self.email= None
+        self.telf= None
         self.preferencias = preferencias or []
         self.historial_adopciones = historial_adopciones or []
         
-    def registrarse(self, nombre, apellido, dni, email, telf):
-        """ pongo que cuando se registre la persona, si no llenaron todos los campos
-        salte un error y que tenga que llenar todos los datos para poder registrarse.
-        
-        Con el return te da un diccionario con todos los datos del usuario.
-        
-        Para usarlo seria por ej:
-        usuario.registrarse("Joaquin", "Fernandez, "47573299", "joaquinfernandezds@gmail.com, "1144005923")"""
-        
-        if not all ([nombre, apellido, dni, email, telf]):
-            raise ValueError("Llenar todos los campos")
+    
+    def registrar_usuario(self, nombre, apellido, dni, email, telf, contraseña):
+        if not all ([nombre, apellido, dni, email, telf, contraseña]):
+            return "Debe llenar todos los campos"
         
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
         self.email = email
         self.telf = telf
+        self.contraseña = contraseña
         
         return {
-            "nombre": self.nombre,
-            "apellido": self.apellido,
-            "dni": self.dni,
-            "email": self.email,
-            "telf": self.telf,
-        }
-        
-    def modificar_datos(self, nuevo_nombre=None, nuevo_apellido=None, nuevo_telf=None, nuevo_dni=None, nuevo_email=None):
-        """ modifico los datos del usuario, actualizo sus atributos.
-        Si hay un nuevo valor para algun atributo, se actualiza y retorna un mensaje diciendo que fueron modificados, y si no se pasa
-        un valor, los datos no se modifican.
-        
-        Por defecto los datos son None
-        
-        por ej para usarlo seria:
-        usuario.modificar_datos(nuevo_nombre="Pepocho", nuevo_apellido="blablabla", y asi con los otros datos)"""
-        
-        if nuevo_nombre:
-            self.nombre = nuevo_nombre
-        if nuevo_apellido:
-            self.apellido = nuevo_apellido
-        if nuevo_dni:
-            self.dni = nuevo_dni
-        if nuevo_email:
-            self.email = nuevo_email
-        if nuevo_telf:
-            self.telf = nuevo_telf
-        return "Los datos fueron modificados correctamente"
-
-    def datosUsuario(self):
-        """organiza los datos del usuario en un diccionario, con su:
-        - nombre, apellido, DNI, email
-        - preferencias del usuario
-        - historial de adopciones
-        
-        esta bueno para acceder a toda la información del usuario por asi decirlo de manera estructurada
-        y reutilizarla en otras partes del programa, como mostrarla en una interfaz
-        gráfica o guardarla en una base de datos ponele
-        
-        para usarlo:
-        datos = usuario.datosUsuario()
-        print(datos["Nombre"])  # accederia al nombre del usuario"""
-        
-        datos = {
             "Nombre": self.nombre,
             "Apellido": self.apellido,
-            "DNI": self.dni,
+            "DNI": self.dni ,
             "Email": self.email,
-            "Preferencias": self.preferencias,
-            "Historial de adopciones": self.historial_adopciones,
+            "Telefono": self.telf,
+            "Contraseña": self.contraseña,
         }
+        
+    
+    def Modificar_Datos(self, nuevo_email=None, nuevo_telefono=None, nueva_contraseña=None):
+        if nuevo_email:
+            self.email = nuevo_email
+        
+        if nuevo_telefono:
+            self.telf = nuevo_telefono
+        
+        if nueva_contraseña:
+            self.contraseña = nueva_contraseña
+        
+        return "Los datos fueron modificados correctamente."
+    
+    def datos_usuario(self):
+        
+        datos = {
+            
+            "Nombre": self.nombre,
+            "Apellido": self.apellido,
+            "Email": self.email,
+            "Teléfono": self.telf,
+            "DNI": self.dni,
+            "Contraseña": self.contraseña,
+        }
+        
         return datos
     
+    def ver_historial(self):
+        if not self.historial_adopciones:
+            return "Este usuario no tiene historial de adopciones."
+        return self.historial_adopciones
+        
+  
+
+class SistemaAdopcion(object):
+    
+    def __init__(self):
+        self.perros = []
+        self.usuarios = []
+         
+    def cargar_perro(self, nuevo_perro):
+        
+        self.perros.append(nuevo_perro)
+        return "El perro se cargo correctamente."
+    
+    def eliminar_perro(self, id_perro):
+        for perro in self.perros:
+            if perro.id == id_perro:
+                self.perros.remove(perro)
+                return "El perro fue removido correctamente."
+        return "Este perro no existe."
+    
+    
+                       
+    def registrar_usuario(self,usuario):
+        self.usuarios.append(usuario)
+        return "El usuario se registro correctamente."
+    
+    
+    
+    def confirmar_adopcion(self, id_perro, usuario):
+        for perro in self.perros:
+            if perro.id == id_perro:
+                if perro.estado_adopcion == "disponible":
+                    perro.cambiar_estado("adoptado")
+                    usuario.historial_adopciones.append(perro)
+                    return "Felicidades!, el perro ha sido adoptado."
+                else:
+                    return "Este perro no esta disponible en este momento para ser adoptado."
+        return "Este perro no existe."
+    
+    
+    def sugerir_perros(self, usuario):
+        sugerencia = []
+        for perros in self.perros:
+            if (
+                perros.temperamento in usuario.preferencias or 
+                perros.raza in usuario.preferencias or 
+                perros.tamaño in usuario.preferencias or
+                perros.edad in usuario.preferencias
+            ):
+                if perros not in sugerencia:
+                    sugerencia.append(perros.mostrar_informacion())
+        if not sugerencia:
+            return "No hay perros que coincidan con las preferencias del usuario."
+        return sugerencia
+        
+    
+    def listado_perros_disponibles(self):
+        disponibles = [perro.mostrar_informacion() for perro in self.perros if perro.estado_adopcion == "disponible"]
+        if not disponibles:
+            return "No hay perros disponibles en este momento."
+        return disponibles
+    
+    def listado_perros_por_estado(self, estado):
+        if estado not in ["disponible", "reservado", "adoptado"]:
+            return "Este estado no es valido."
+        
+        estado_perros= [perro.mostrar_informacion() for perro in self.perros if perro.estado_adopcion == estado]
+        return estado_perros
+    
+    def listado_perros_por_usuario(self, usuario):
+        for usua in self.usuarios:
+            if usua.nombre == usuario.nombre and usua.dni == usuario.dni:
+                return usua.ver_historial()
+        return "Este usuario no existe o no tiene historial de adopciones."
